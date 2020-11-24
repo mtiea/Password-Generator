773fc4c4b6da5f2705ec72543e9f8e010302fa32
Magdi Tiea <magditiea@gmail.com>
2020-11-24 19:05:47 -0400

    special-case/upper/lower-case array added

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// array of special characters
+var specialcharacters = ['!','@','#','$','%','^','&','*','(',')','-','_','=','+','?', '/','{','}',';','>','<']
+
+// array of lowercase letters
+var lowercaseletters = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m']
+
+// array of uppercaseletters
+var uppercaseletters = ['Q','W','E','R','T','Y','Y','U','I','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M']
+
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
