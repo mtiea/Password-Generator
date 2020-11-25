bddfa9994df3f0a271e1bccf7189a28687e06906
Magdi Tiea <magditiea@gmail.com>
2020-11-25 15:24:19 -0400

    condition statements for password length added

--- script.js
+++ script.js
@@ -7,9 +7,6 @@ var lowercaseletters = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f',
 // array of uppercaseletters
 var uppercaseletters = ['Q','W','E','R','T','Y','Y','U','I','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M']
 
-
-
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -25,3 +22,23 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+// On generate button open prompt question
+
+function getpasswordoptions(){
+var lenth = parseInt(prompt("How many characters would you like your password to contain?"))
+}
+// Condition to check if answer is a number
+if (isNaN)(length == true) {
+    alert('Password length must be provided as a number'); 
+    
+}
+// Condition to check if answeris less than 8 characters, if less than 8 then an alert will keep showing
+ if (length < 8) {
+   alert("Password length must be 8 characters or more"); return;
+ }
+
+// Condition to check if answer is less than 128 characters, if less then an alert will show
+if (length > 128) {
+  alert("Password must be 128 characters or less")
+}
