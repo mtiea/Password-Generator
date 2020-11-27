a92d3e9fa851b767c3e6dc1624f74e365486da78
Magdi Tiea <magditiea@gmail.com>
2020-11-26 22:45:04 -0400

    event listener modified

--- script.js
+++ script.js
@@ -7,38 +7,47 @@ var lowercaseletters = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f',
 // array of uppercaseletters
 var uppercaseletters = ['Q','W','E','R','T','Y','Y','U','I','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M']
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+//Onclick event when button is pressed
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+generateBtn.addEventListener("click", passwordChar);
 
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 
 // On generate button open prompt question
+function passwordChar()   {
+  var length = parseInt(
+    prompt('How many characters would you like your password to contain?') 
+    );
 
-function getpasswordoptions(){
-var lenth = parseInt(prompt("How many characters would you like your password to contain?"))
-}
 // Condition to check if answer is a number
-if (isNaN)(length == true) {
+if (isNaN(length) === true) {
     alert('Password length must be provided as a number'); 
+    return;
     
 }
-// Condition to check if answeris less than 8 characters, if less than 8 then an alert will keep showing
+// Condition to check if answer is less than 8 characters, if less than 8 then an alert will keep showing
  if (length < 8) {
-   alert("Password length must be 8 characters or more"); return;
+   alert('Password length must be 8 characters or more'); 
+   return;
  }
 
 // Condition to check if answer is less than 128 characters, if less then an alert will show
 if (length > 128) {
-  alert("Password must be 128 characters or less")
+  alert("Password must be 128 characters or less");
+  return;
 }
+
+// Condition to check if you want special characters
+
+
+
+
+// Condition to check if you want numeric characters
+
+
+
+//Condition to check if you want lowercase characters
+
+
+
+//Condition to check if you want uppercase characters
