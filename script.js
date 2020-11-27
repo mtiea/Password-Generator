c6cddc51e52506fc2d3a625d233aedcb52197efc
Magdi Tiea <magditiea@gmail.com>
2020-11-27 17:44:55 -0400

    confirm pop-up messages added

--- script.js
+++ script.js
@@ -7,11 +7,22 @@ var lowercaseletters = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f',
 // array of uppercaseletters
 var uppercaseletters = ['Q','W','E','R','T','Y','Y','U','I','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M']
 
+// array of numeric characters
+var numericcharacters = ['1','2','3','4','5','6','7'.'8','9']
+
 //Onclick event when button is pressed
+generateBtn.addEventListener('click' passwordChar;)
 
-generateBtn.addEventListener("click", passwordChar);
+// referance to the #generate element
+var generateBtn = document.querySelector("#generate")
 
+// write password to the password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
 
+  passwordText.value = password;
+}
 
 // On generate button open prompt question
 function passwordChar()   {
@@ -34,20 +45,21 @@ if (isNaN(length) === true) {
 // Condition to check if answer is less than 128 characters, if less then an alert will show
 if (length > 128) {
   alert("Password must be 128 characters or less");
-  return;
 }
 
 // Condition to check if you want special characters
+_var specialcharacters =  confirm("Click OK to confirm using special characters";
 
 
 
 
 // Condition to check if you want numeric characters
-
+_var numericcharacters = confirm("Click OK to confirm using numeric characters");
 
 
 //Condition to check if you want lowercase characters
-
+_var lowercaseletters; = confirm("Click OK to confirm using numeric characters");
 
 
 //Condition to check if you want uppercase characters
+_var uppercaseletters; = confirm("Click OK to confirm using uppercase letters");
